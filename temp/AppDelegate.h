7df8d00cfd4972ce333aa18c1bc4beff0ff0e664
Christian D Tuen <christiandt@gmail.com>
2014-03-01 16:43:18 -0800

    Initial Commit

---
+++ temp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by Christian DT on 01/03/2014.
+//  Copyright (c) 2014 Christian DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ temp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  temp
+//
+//  Created by Christian DT on 01/03/2014.
+//  Copyright (c) 2014 Christian DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
